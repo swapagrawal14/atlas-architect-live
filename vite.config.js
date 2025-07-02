4e430d2eed69094e08202f9523a00538c5788559
bhavishy2801 <bhavishyrocker2801@gmail.com>
2025-07-02 20:25:39 +0530

    Updated changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/atlas-architect-live/',  // <-- your GitHub repo name here, with leading and trailing slash
-})
+  base: '/atlas-architect-live/', // <-- Use your repo name here!
+});
