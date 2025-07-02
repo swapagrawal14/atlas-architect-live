10a0a483e7adea295b7cfa3829e6e943c3e55f04
bhavishy2801 <bhavishyrocker2801@gmail.com>
2025-07-02 19:13:07 +0530

    Updated changes

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasmPack from 'vite-plugin-wasm-pack';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    wasmPack(['../generator']) // path to your Rust WASM crate
+  ]
 });
